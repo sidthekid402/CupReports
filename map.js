3bf2b773e09d2de2f5984cc30cd2c34e0c813fe0
sidthekid402 <sidthekid402@gmail.com>
2017-04-12 13:57:05 -0700

    fix

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var database = firebase.database();
 
 var map;
@@ -58,12 +58,4 @@ function placeMarker(location) {
     });
 }
 }
- 
-=======
 
-  var script = document.createElement('script');
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-  document.getElementsByTagName('head')[0].appendChild(script);
->>>>>>> origin/master
